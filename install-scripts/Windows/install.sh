f3327699c95020996f9b529480abeb14dff8099f
Thomas Caramn <thomascarman@users.noreply.github.com>
2022-06-09 11:41:45 -0700

    Add Install Script for windows to get git-completion

---
+++ install-scripts/Windows/install.sh
@@ -0,0 +1,16 @@
+log_file=~/install_progress_log.txt
+
+# ---
+# Install git-completion and git-prompt
+# ---
+cd ~/
+curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+mv ~/git-completion.bash ~/.git-completion.bash
+curl https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+echo "git-completion and git-prompt Installed and Configured" >> $log_file
+
+echo -e "\n====== Summary ======\n"
+cat $log_file
+echo
+echo "Enjoy"
+rm $log_file
