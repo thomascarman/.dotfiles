04fc70a90f74c2f7d6e861416057e943d60d07d5
Thomas Carman <thomascarman@users.noreply.github.com>
2022-08-04 14:41:30 -0700

    Stop Message from pop up after image

--- message.sh
+++ message.sh
@@ -2,26 +2,26 @@
 
 cat ~/.dotfiles/welcomeMessage.txt
 
-while true; do
-
-read -p "Do you want to open up your wiki? (Y/n) " yn
-
-if [ "$yn" = "" ]; then
-    yn="Y"
-fi
-
-case $yn in
-    [yY] ) echo ok, Opening...;
-        cd ~/.vault;
-        sleep 1;
-        clear;
-        vim +VimwikiIndex;
-        break;;
-    [nN] ) echo no problem;
-        sleep 1;
-        clear;
-        break;;
-    * ) echo invalid response;;
-esac
-
-done
+# while true; do
+# 
+# read -p "Do you want to open up your wiki? (Y/n) " yn
+# 
+# if [ "$yn" = "" ]; then
+#     yn="Y"
+# fi
+# 
+# case $yn in
+#     [yY] ) echo ok, Opening...;
+#         cd ~/.vault;
+#         sleep 1;
+#         clear;
+#         vim +VimwikiIndex;
+#         break;;
+#     [nN] ) echo no problem;
+#         sleep 1;
+#         clear;
+#         break;;
+#     * ) echo invalid response;;
+# esac
+# 
+# done
