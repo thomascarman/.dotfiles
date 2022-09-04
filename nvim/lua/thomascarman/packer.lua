8f94a322aa6a1051584361c86eb77bf4e3b368f9
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-04 09:13:29 -0700

    Change format of use in packer.lua

--- nvim/lua/thomascarman/packer.lua
+++ nvim/lua/thomascarman/packer.lua
@@ -1,6 +1,6 @@
 return require('packer').startup(function()
     -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+    use { 'wbthomason/packer.nvim' }
 
     -- colorscheme
     use("gruvbox-community/gruvbox")
