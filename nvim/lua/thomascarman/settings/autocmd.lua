320366c75fa16c8e37fd6ca03afaaaf1f805a68a
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-08 08:54:07 -0700

    Create autocmd settings for Adding non plugin autocmds

---
+++ nvim/lua/thomascarman/settings/autocmd.lua
@@ -0,0 +1,16 @@
+local augroup = require('thomascarman.utils.augroup')
+
+-- Setup format for svelte to show as html
+augroup('sveltegroup')(function(autocmd)
+    autocmd({ 'BufNewFile', 'BufRead' }, { pattern = '*.svelte'}, 'set ft=html')
+end)
+
+augroup('vimwikigroup')(function(autocmd)
+    autocmd({ 'BufNewFile', 'BufRead' }, { pattern = 'diary.md'}, 'VimwikiDiaryGenerateLinks')
+    autocmd({ 'BufNewFile', 'BufRead' }, { pattern = 'Tags.md'}, 'VimwikiGenerateTagLinks')
+     -- still needs some work tosetup somesort of template system
+    --autocmd({ 'BufNewFile' }, { pattern = '*/diary/*.md'}, function()
+    --    vim.cmd[[%!echo diary]]
+    --end)
+end)
+
