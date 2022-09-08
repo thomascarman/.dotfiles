bf8c7bb0a621b724c0eb365584a9b5c77144da7d
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-08 09:55:19 -0700

    Update Init on plugin Settings init.lua

--- nvim/lua/thomascarman/settings/plugins/init.lua
+++ nvim/lua/thomascarman/settings/plugins/init.lua
@@ -1,7 +1,7 @@
 require 'thomascarman.settings.plugins.startup'
 require 'thomascarman.settings.plugins.lualine'
 require 'thomascarman.settings.plugins.marks'
-require 'thomascarman.settings.plugins.tree'
+require 'thomascarman.settings.plugins.nvimtree'
 require 'thomascarman.settings.plugins.toggleterm'
 require 'thomascarman.settings.plugins.telescope'
 require 'thomascarman.settings.plugins.hop'
