448b192c1ac2b32629669b01d87e44ddc68bfb9e
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-08 09:20:35 -0700

    Add Rest of Settings to the Plugins Init

--- nvim/lua/thomascarman/settings/plugins/init.lua
+++ nvim/lua/thomascarman/settings/plugins/init.lua
@@ -1,1 +1,9 @@
+require 'thomascarman.settings.plugins.startup'
+require 'thomascarman.settings.plugins.lualine'
+require 'thomascarman.settings.plugins.marks'
 require 'thomascarman.settings.plugins.tree'
+require 'thomascarman.settings.plugins.toggleterm'
+require 'thomascarman.settings.plugins.telescope'
+require 'thomascarman.settings.plugins.hop'
+require 'thomascarman.settings.plugins.gitsigns'
+
