4abb6a28fe86e6b3f4ee4302a3d2b9d174b8c5f0
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-09 00:09:47 -0700

    Update Session to stop Auto Restore and update lualine

--- nvim/lua/thomascarman/settings/plugins/sessions.lua
+++ nvim/lua/thomascarman/settings/plugins/sessions.lua
@@ -4,6 +4,9 @@ require('auto-session').setup {
     auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
     auto_session_enabled = true,
     auto_save_enabled = true,
-    auto_restore_enabled = true,
+    auto_restore_enabled = false,
     auto_session_create_enabled = false,
+    post_cwd_changed_hook = function ()
+        require('lualine').refresh()
+    end,
 }
