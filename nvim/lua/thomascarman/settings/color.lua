4428ab34a92e8508a585f5ffd16513122dafa8c3
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-09 00:10:06 -0700

    Change transparent to true for color

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -4,6 +4,6 @@ vim.cmd[[highlight clear SignColumn]]
 
 local status_ok, transparent = pcall(require, "transparent")
 if status_ok then
-    transparent.setup { enable = false }
+    transparent.setup { enable = true }
 end
 
