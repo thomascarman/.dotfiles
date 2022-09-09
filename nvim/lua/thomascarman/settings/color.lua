e64a0fa831ad1706caaaaf17508ce99429401a91
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-08 17:25:56 -0700

    Add pcall To the transparent Plugin setup

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -2,4 +2,8 @@ vim.opt.background = "dark"
 vim.cmd[[colorscheme gruvbox]]
 vim.cmd[[highlight clear SignColumn]]
 
-require('transparent').setup { enable = false }
+local status_ok, transparent = pcall(require, "transparent")
+if status_ok then
+    transparent.setup { enable = false }
+end
+
