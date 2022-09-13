de4bc35a4602c0ea1a2a61e08f53101c7eea65fd
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-13 09:58:06 -0700

    Remove Auto Command for Svelte As HTML

--- nvim/lua/thomascarman/settings/autocmd.lua
+++ nvim/lua/thomascarman/settings/autocmd.lua
@@ -1,10 +1,5 @@
 local augroup = require('thomascarman.utils.augroup')
 
--- Setup format for svelte to show as html
-augroup('sveltegroup')(function(autocmd)
-    autocmd({ 'BufNewFile', 'BufRead' }, { pattern = '*.svelte'}, 'set ft=html')
-end)
-
 augroup('vimwikigroup')(function(autocmd)
     autocmd({ 'BufNewFile', 'BufRead' }, { pattern = 'diary.md'}, 'VimwikiDiaryGenerateLinks')
     autocmd({ 'BufNewFile', 'BufRead' }, { pattern = 'Tags.md'}, 'VimwikiGenerateTagLinks')
