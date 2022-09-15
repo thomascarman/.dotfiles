750d8431b7a9a9f5a315d74fccbff56b29979750
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-15 15:40:48 -0700

    Remove Transparent Configs

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -1,15 +1,6 @@
 vim.opt.background = "dark"
-vim.cmd[[colorscheme gruvbox]]
-vim.cmd[[highlight clear SignColumn]]
-
-local status_ok, transparent = pcall(require, "transparent")
-local enable = false
-if vim.g.neovide then
-    enable = true
-end
-if status_ok then
-    transparent.setup { enable = enable }
-end
+vim.cmd("colorscheme gruvbox")
+vim.cmd('highlight clear SignColumn')
 
 local colorizer_ok, colorizer = pcall(require, "colorizer")
 if colorizer_ok then
