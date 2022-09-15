3bbcec0fe0bc740cc5b7b4d2c6be05c1762bb471
Thomas Carman <thomascarman@users.noreply.github.com>
2022-09-15 12:20:21 -0700

    Update Bookmarks To check if Directories Exists

--- nvim/lua/thomascarman/bookmarks.lua
+++ nvim/lua/thomascarman/bookmarks.lua
@@ -3,15 +3,25 @@ if not utils_ok then
     return
 end
 
-local nvimConfig = '~/.config/nvim/'
+local nvimConfig = '~/.config/nvim'
 if utils.getOSName() == 'Windows' then
-    nvimConfig = '~/AppData/Local/nvim/'
+    nvimConfig = '~/AppData/Local/nvim'
 end
 
-vim.g.startup_bookmarks = {
-    ["D"] = '~/.dotfiles/',
-    ["N"] = nvimConfig,
+local bookmarks = {
+    ["D"] = '~/.dotfiles',
     ["V"] = '~/.vault',
+    ["N"] = nvimConfig,
     ["W"] = '~/workspace',
     ["S"] = '~/Sketches',
 }
+
+for key, value in pairs(bookmarks) do
+    if not utils.exists(value) then
+        -- Remove Key from bookmarks in not exists
+        bookmarks[key] = nil
+    end
+end
+
+vim.g.startup_bookmarks = bookmarks
+
