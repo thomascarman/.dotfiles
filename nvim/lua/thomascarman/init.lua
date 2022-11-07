762798cae034080359b760a06091aac8dde5ab98
Thomas Carman <thomascarman@users.noreply.github.com>
2022-11-07 12:38:52 -0800

    Update init to Check for global VSCode Used in VSCode Nvim Extention

--- nvim/lua/thomascarman/init.lua
+++ nvim/lua/thomascarman/init.lua
@@ -1,8 +1,13 @@
-require 'thomascarman.neovide'
-require 'thomascarman.settings'
-require 'thomascarman.bookmarks'
-require 'thomascarman.plugins'
-require 'thomascarman.settings.plugins'
-require 'thomascarman.cmp'
-require 'thomascarman.lsp'
-
+if vim.g.vscode then
+    require 'thomascarman.settings'
+    require 'thomascarman.vscode'
+    require 'thomascarman.settings.plugins.hop'
+else
+    require 'thomascarman.neovide'
+    require 'thomascarman.settings'
+    require 'thomascarman.bookmarks'
+    require 'thomascarman.plugins'
+    require 'thomascarman.settings.plugins'
+    require 'thomascarman.cmp'
+    require 'thomascarman.lsp'
+end
