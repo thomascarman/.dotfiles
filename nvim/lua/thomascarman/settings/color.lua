8fe411e61f74d2e532fb10516336b8657f1e6244
Thomas Carman <thomascarman@users.noreply.github.com>
2022-12-29 08:13:32 -0800

    Add Remove backaground for transparent

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -1,6 +1,7 @@
 vim.opt.background = "dark"
 vim.cmd("colorscheme gruvbox")
 vim.cmd('highlight clear SignColumn')
+vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
 
 local colorizer_ok, colorizer = pcall(require, "colorizer")
 if colorizer_ok then
