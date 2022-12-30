b1e63aa5e22fe7584f8eedd43020f5dc7145a1e4
Thomas Carman <thomascarman@users.noreply.github.com>
2022-12-30 08:10:18 -0800

    Update AutoSessions Settings with Pcall

--- nvim/lua/thomascarman/settings/plugins/sessions.lua
+++ nvim/lua/thomascarman/settings/plugins/sessions.lua
@@ -1,4 +1,9 @@
-require('auto-session').setup {
+local auto_session_ok, auto_session = pcall(require, 'auto-session')
+if not auto_session_ok then
+    return
+end
+
+auto_session.setup {
     log_level = 'info',
     auto_session_enable_last_session = false,
     auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
