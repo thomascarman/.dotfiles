b9af757ff0e842b83d35473bef3bd195bdf37f88
Thomas Carman <thomascarman@users.noreply.github.com>
2022-12-30 08:38:59 -0800

    Add ensure_packer to Auto Install and Sync Packer

--- nvim/lua/thomascarman/plugins.lua
+++ nvim/lua/thomascarman/plugins.lua
@@ -1,3 +1,17 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+-- Run to check if packer exist and install if not
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use { 'wbthomason/packer.nvim' }
@@ -105,4 +119,8 @@ return require('packer').startup(function(use)
     use { 'seandewar/nvimesweeper' }
     use { 'ThePrimeagen/vim-be-good' }
     use { 'seandewar/killersheep.nvim' }
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
