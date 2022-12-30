97b5655ae6ff822116681b20db51da17dcf3cd1e
Thomas Carman <thomascarman@users.noreply.github.com>
2022-12-30 09:32:15 -0800

    Add Check to Colors to check for Dir

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -1,8 +1,16 @@
 vim.opt.background = "dark"
-vim.cmd("colorscheme gruvbox")
+
+-- Set color scheme
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/gruvbox'
+if vim.fn.empty(vim.fn.glob(install_path)) <= 0 then
+    vim.cmd[[colorscheme gruvbox]]
+end
+
 vim.cmd('highlight clear SignColumn')
 vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
 
+
+-- Colorizer used to show Hex with background
 local colorizer_ok, colorizer = pcall(require, "colorizer")
 if colorizer_ok then
     colorizer.setup()
