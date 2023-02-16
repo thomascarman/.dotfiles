721bbcf3fc0c270732031bf1c1dd36e0e2ec93ec
Thomas Carman <thomascarman@users.noreply.github.com>
2023-02-16 10:15:55 -0800

    Add Harpoon Config and Update requires

--- nvim/lua/thomascarman/settings/plugins/init.lua
+++ nvim/lua/thomascarman/settings/plugins/init.lua
@@ -1,6 +1,5 @@
 require 'thomascarman.settings.plugins.startup'
 require 'thomascarman.settings.plugins.lualine'
-require 'thomascarman.settings.plugins.sessions'
 require 'thomascarman.settings.plugins.marks'
 require 'thomascarman.settings.plugins.nvimtree'
 require 'thomascarman.settings.plugins.toggleterm'
@@ -9,4 +8,5 @@ require 'thomascarman.settings.plugins.hop'
 require 'thomascarman.settings.plugins.gitsigns'
 require 'thomascarman.settings.plugins.iconpicker'
 require 'thomascarman.settings.plugins.treesitter'
-
+require 'thomascarman.settings.plugins.harpoon'
+require 'thomascarman.settings.plugins.sessions'
