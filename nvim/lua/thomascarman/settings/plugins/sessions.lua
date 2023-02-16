3ff83ee9a69f1d5a4e8c1d9a6ad43f4d97cde16f
Thomas Carman <thomascarman@users.noreply.github.com>
2023-02-16 10:16:17 -0800

    Update Sessions.lua for Sessions mangager

--- nvim/lua/thomascarman/settings/plugins/sessions.lua
+++ nvim/lua/thomascarman/settings/plugins/sessions.lua
@@ -1,17 +1,40 @@
-local auto_session_ok, auto_session = pcall(require, 'auto-session')
-if not auto_session_ok then
+local session_ok, session_manager = pcall(require, 'session_manager')
+if not session_ok then
     return
 end
 
-auto_session.setup {
-    log_level = 'info',
-    auto_session_enable_last_session = false,
-    auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
-    auto_session_enabled = true,
-    auto_save_enabled = true,
-    auto_restore_enabled = false,
-    auto_session_create_enabled = false,
-    post_cwd_changed_hook = function ()
-        require('lualine').refresh()
-    end,
-}
+session_manager.setup({
+    sessions_dir = vim.fn.stdpath('data') .. '/sessions',
+    path_replacer = '__',
+    colon_replacer = '++',
+    autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+    autosave_last_session = true,
+    autosave_ignore_not_normal = true,
+    autosave_ignore_dirs = {},
+    autosave_ignore_filetypes = {
+        'gitcommit',
+    },
+    autosave_ignore_buftypes = {},
+    autosave_only_in_session = true,
+    max_path_length = 80,
+})
+
+local config_group = vim.api.nvim_create_augroup('SessionGroup', {})
+
+vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
+  group = config_group,
+  callback = function ()
+    if vim.bo.filetype ~= 'git'
+      and not vim.bo.filetype ~= 'gitcommit'
+      then session_manager.autosave_session() end
+  end
+})
+
+local Remap = require('thomascarman.utils').remap
+
+local nnoremap = Remap.nnoremap
+
+nnoremap('<leader>sl', ':SessionManager load_session<CR>')
+nnoremap('<leader>ss', ':SessionManager save_current_session<CR>')
+nnoremap('<leader>sd', ':SessionManager delete_session<CR>')
+
