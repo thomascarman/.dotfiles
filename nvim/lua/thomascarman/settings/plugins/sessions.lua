0fdbbe0e36b6f399d54e5e994a5503fd162fab99
Thomas Carman <thomascarman@users.noreply.github.com>
2023-03-23 08:35:28 -0700

    Disable the Autoload of Sessions

--- nvim/lua/thomascarman/settings/plugins/sessions.lua
+++ nvim/lua/thomascarman/settings/plugins/sessions.lua
@@ -7,7 +7,7 @@ session_manager.setup({
     sessions_dir = vim.fn.stdpath('data') .. '/sessions',
     path_replacer = '__',
     colon_replacer = '++',
-    autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
+    autoload_mode = require('session_manager.config').AutoloadMode.Disabled,
     autosave_last_session = true,
     autosave_ignore_not_normal = true,
     autosave_ignore_dirs = {},
