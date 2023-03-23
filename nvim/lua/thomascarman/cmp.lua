4ba39b5dc93861b4a93081f1a08411e8218cf68e
Thomas Carman <thomascarman@users.noreply.github.com>
2023-03-22 20:29:43 -0700

    Update cmp to use C-Tab to use

--- nvim/lua/thomascarman/cmp.lua
+++ nvim/lua/thomascarman/cmp.lua
@@ -63,7 +63,7 @@ cmp.setup({
 		-- Accept currently selected item. If none selected, `select` first item.
 		-- Set `select` to `false` to only confirm explicitly selected items.
 		["<CR>"] = cmp.mapping.confirm({ select = true }),
-		["<Tab>"] = cmp.mapping(function(fallback)
+		["<C-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
 			elseif luasnip.expandable() then
@@ -79,7 +79,7 @@ cmp.setup({
 			"i",
 			"s",
 		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
+		["<C-S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
 			elseif luasnip.jumpable(-1) then
