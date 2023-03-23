50aed3c25489b1e21922f3b8c488c151efd14bea
Thomas Carman <thomascarman@users.noreply.github.com>
2023-03-22 20:24:26 -0700

    Comment out the icon insert in insert mode

--- nvim/lua/thomascarman/settings/plugins/iconpicker.lua
+++ nvim/lua/thomascarman/settings/plugins/iconpicker.lua
@@ -11,4 +11,5 @@ iconpicker.setup {
 
 vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
 vim.keymap.set("n", "<Leader><Leader>y", "<cmd>IconPickerYank<cr>", opts) --> Yank the selected icon into register
-vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
+-- Issue causing <Tab> to trigger this confilting with Copilot
+-- vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
