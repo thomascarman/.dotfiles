73602210d2055dfeedc57df5c9187c8b90398cf5
Thomas Carman <thomascarman@users.noreply.github.com>
2023-04-13 12:36:22 -0700

    Update Color Settings to Set Additonal Props and Add pcall on elements

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -1,17 +1,20 @@
-vim.opt.background = "dark"
-
 -- Set color scheme
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/gruvbox'
 if vim.fn.empty(vim.fn.glob(install_path)) <= 0 then
-    vim.cmd[[colorscheme gruvbox]]
+    -- Only doing pcalls here because i cant figure out right now why treesetter
+    -- doesnt like calling vim.cmd
+    pcall(vim.cmd, 'colorscheme gruvbox')
+    pcall(vim.cmd, 'highlight clear SignColumn')
+    pcall(vim.cmd, 'highlight Normal guibg=NONE ctermbg=NONE')
 end
-
-vim.cmd('highlight clear SignColumn')
-vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
-
 
 -- Colorizer used to show Hex with background
 local colorizer_ok, colorizer = pcall(require, "colorizer")
 if colorizer_ok then
     colorizer.setup()
 end
+
+vim.g.gruvbox_contrast_light = 'soft'
+vim.g.gruvbox_contrast_dark = 'medium'
+
+vim.opt.background = "light"
