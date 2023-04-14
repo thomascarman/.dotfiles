98a4ccbc034cfdc06bbc148d39ef8e498bdf0e8a
Thomas Carman <thomascarman@users.noreply.github.com>
2023-04-14 11:50:02 -0700

    Add Telescope Extension gh

--- nvim/lua/thomascarman/settings/plugins/telescope.lua
+++ nvim/lua/thomascarman/settings/plugins/telescope.lua
@@ -16,6 +16,7 @@ telescope.setup {
 
 telescope.load_extension 'file_browser'
 telescope.load_extension 'harpoon'
+telescope.load_extension 'gh'
 
 local Remap = require('thomascarman.utils').remap
 
