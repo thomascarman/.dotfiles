cfd48281d1fc696b88aebe3717bad8bb10e80df5
Thomas Carman <thomascarman@users.noreply.github.com>
2024-01-30 08:48:40 -0800

    Move Highlight Props to the within the pcall

--- nvim/lua/thomascarman/settings/color.lua
+++ nvim/lua/thomascarman/settings/color.lua
@@ -4,6 +4,8 @@ if vim.fn.empty(vim.fn.glob(install_path)) <= 0 then
     -- Only doing pcalls here because i cant figure out right now why treesetter
     -- doesnt like calling vim.cmd
     pcall(vim.cmd, 'colorscheme gruvbox')
+    pcall(vim.cmd, 'highlight clear SignColumn')
+    pcall(vim.cmd, 'highlight Normal guibg=NONE ctermbg=NONE')
 end
 
 -- Colorizer used to show Hex with background
@@ -16,6 +18,3 @@ vim.g.gruvbox_contrast_light = 'soft'
 vim.g.gruvbox_contrast_dark = 'medium'
 
 vim.opt.background = "dark"
-
-pcall(vim.cmd, 'highlight Normal guibg=NONE ctermbg=NONE')
-pcall(vim.cmd, 'highlight clear SignColumn')
