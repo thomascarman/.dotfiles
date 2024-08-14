c0042963fba407a4471683ed2cbe89d66a92d258
Thomas Carman <thomascarman@users.noreply.github.com>
2024-08-14 11:10:51 -0700

    Format the Comments Settings

--- nvim/lua/thomascarman/settings/plugins/comments.lua
+++ nvim/lua/thomascarman/settings/plugins/comments.lua
@@ -1,6 +1,6 @@
-local comments_ok, comments = pcall(require, 'todo-comments')
+local comments_ok, comments = pcall(require, "todo-comments")
 if not comments_ok then
-    return
+	return
 end
 
 -- FIX:
@@ -12,22 +12,22 @@ end
 -- TEST:
 -- BUG:
 
-comments.setup {
-    keywords = {
-        FIX = {
-          icon = "", -- icon used for the sign, and in search results
-          color = "error", -- can be a hex color, or a named color (see below)
-          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-          -- signs = false, -- configure signs for some keywords individually
-        },
-        TODO = { icon = "", color = "info" },
-        HACK = { icon = "", color = "warning" },
-        WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
-        PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-        NOTE = { icon = "", color = "hint", alt = { "INFO" } },
-        TEST = { icon = "ﭧ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-    },
-    search = {
-        command = "vimgrep"
-    },
-}
+comments.setup({
+	keywords = {
+		FIX = {
+			icon = "", -- icon used for the sign, and in search results
+			color = "error", -- can be a hex color, or a named color (see below)
+			alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+			-- signs = false, -- configure signs for some keywords individually
+		},
+		TODO = { icon = "", color = "info" },
+		HACK = { icon = "", color = "warning" },
+		WARN = { icon = "", color = "warning", alt = { "WARNING", "XXX" } },
+		PERF = { icon = "", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+		NOTE = { icon = "", color = "hint", alt = { "INFO" } },
+		TEST = { icon = "ﭧ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+	},
+	search = {
+		command = "vimgrep",
+	},
+})
