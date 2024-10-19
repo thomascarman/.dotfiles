15a21937a5671a8f8345936b239f3677fe365456
Thomas Carman <thomascarman@users.noreply.github.com>
2024-10-19 07:46:19 -0700

    Update Plugin and Configs

--- nvim/lua/nvChad-custom/plugins.lua
+++ nvim/lua/nvChad-custom/plugins.lua
@@ -1,131 +1,137 @@
-local overrides = require "configs.overrides"
+local overrides = require("configs.overrides")
 
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      -- require "plugins.configs.lspconfig"
-      require "configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			-- require "plugins.configs.lspconfig"
+			require("configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason,
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  -- { "github/copilot.vim", lazy = false },
-  { "MunifTanjim/prettier.nvim", lazy = false },
-  {
-    "kylechui/nvim-surround",
-    lazy = false,
-    config = function()
-      require("nvim-surround").setup()
-    end,
-  },
+	-- Install a plugin
+	{ "github/copilot.vim", lazy = false },
+	{ "MunifTanjim/prettier.nvim", lazy = false },
+	{
+		"kylechui/nvim-surround",
+		lazy = false,
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
 
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  {
-    "phaazon/hop.nvim",
-    branch = "v2", -- optional but strongly recommended
-    config = function()
-      require("hop").setup {
-        keys = "abcdefghijklmnopqrstuvwxyz;",
-        case_insensitive = true,
-        multi_windows = true,
-        jump_on_sole_occurrence = false,
-        create_hl_autocmd = false,
-        uppercase_labels = true,
-      }
-    end,
-  },
+	{
+		"phaazon/hop.nvim",
+		branch = "v2", -- optional but strongly recommended
+		config = function()
+			require("hop").setup({
+				keys = "abcdefghijklmnopqrstuvwxyz;",
+				case_insensitive = true,
+				multi_windows = true,
+				jump_on_sole_occurrence = false,
+				create_hl_autocmd = false,
+				uppercase_labels = true,
+			})
+		end,
+	},
 
-  {
-    "ThePrimeagen/harpoon",
-    lazy = false,
-    requires = { "nvim-lua/plenary.nvim" },
-  },
+	{
+		"ThePrimeagen/harpoon",
+		lazy = false,
+		requires = { "nvim-lua/plenary.nvim" },
+	},
 
-  {
-    "xiyaowong/telescope-emoji.nvim",
-    lazy = false,
-    config = function()
-      require("telescope").load_extension "emoji"
-    end,
-  },
+	{
+		"xiyaowong/telescope-emoji.nvim",
+		lazy = false,
+		config = function()
+			require("telescope").load_extension("emoji")
+		end,
+	},
 
-  {
-    "folke/todo-comments.nvim",
-    lazy = false,
-    config = function()
-      require "configs.comments"
-    end,
-  },
+	{
+		"folke/todo-comments.nvim",
+		lazy = false,
+		config = function()
+			require("configs.comments")
+		end,
+	},
 
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		config = function()
+			require("configs.conform")
+		end,
+	},
+	{
+		"vimwiki/vimwiki",
+		lazy = false,
+		init = function()
+			vim.g.vimwiki_list = {
+				{
+					path = "~/.vault/docs",
+					syntax = "markdown",
+					ext = ".md",
+					auto_tags = 1,
+					auto_generate_tags = 0,
+					auto_diary_index = 1,
+				},
+			}
+			vim.g.vimwiki_hl_headers = 1
+			vim.g.vimwiki_hl_cb_checked = 2
+		end,
+	},
+	{
+		"ElPiloto/telescope-vimwiki.nvim",
+		lazy = false,
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		lazy = false,
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("render-markdown").setup({
+				file_types = { "markdown", "vimwiki" },
+			})
+			vim.treesitter.language.register("markdown", "vimwiki")
+		end,
+	},
 
-  -- {
-  -- 	"vimwiki/vimwiki",
-  -- 	lazy = false,
-  -- 	config = function()
-  -- 		vim.g.vimwiki_list = {
-  -- 			{
-  -- 				path = "~/.vault/docs/",
-  -- 				syntax = "markdown",
-  -- 				ext = ".md",
-  -- 				auto_tags = 1,
-  -- 				auto_generate_tags = 0,
-  -- 				auto_diary_index = 1,
-  -- 			},
-  -- 		}
-  -- 		vim.g.vimwiki_hl_headers = 1
-  -- 		vim.g.vimwiki_hl_cb_checked = 2
-  -- 	end,
-  -- },
-
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
-
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	{ "alec-gibson/nvim-tetris", lazy = false },
+	{ "seandewar/nvimesweeper", lazy = false },
+	{ "ThePrimeagen/vim-be-good", lazy = false },
+	{ "seandewar/killersheep.nvim", lazy = false },
 }
 
 return plugins
